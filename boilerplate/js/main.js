c38ef29e214531fdffa600a0a51133988a704d84
smichalski3 <smichalski0302@gmail.com>
2023-02-24 21:12:50 -0600

    activity 6 - sequencing, retrieve, and prop. symbols

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,7 +1,6 @@
-// Add all scripts to the JS folder
-
 //declare map variable globally so all functions have access
 var map;
+var minValue;
 
 //function to instantiate the Leaflet map
 function createMap(){
@@ -9,49 +8,238 @@ function createMap(){
     //create the map
     map = L.map('map', {
         center: [0, 0],
-        zoom: 4
+        zoom: 1
     });
 
     //add OSM base tilelayer
-
-   // L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
-	    //attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	    //apikey: '<your apikey>',
-	    //maxZoom: 22
-//}).addTo(map);
-
-
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
+    L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/cl9zwm0n1002t14k651t6gs8n/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
 
     //call getData function
     getData(map);
 };
 
-//function to retrieve the data and place it on the map
-function getData(map){
-    fetch("data/museums.geojson")
-		.then(function(response){
-			return response.json();
-		})
-		.then(function(response){
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff0000",
-                color: "#fff",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
+// function to calculate minimum value of data
+function calcMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var museum of data.features){
+        //loop through each year
+        for(var year = 2015; year <= 2021; year+=1){
+              //get visitation for current year
+              var value = museum.properties["visitor_"+ String(year)];
+              //add value to array
+              allValues.push(value);
+        }
+    }
+
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+};
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 1;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+
+
+//function to convert markers to circle markers and add popups
+function pointToLayer(feature, latlng,attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    //check
+    console.log(attribute);
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>Museum:</b> " + feature.properties.Museum + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Visitation in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+          offset: new L.Point(0,-options.radius)
+      });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+      console.log("here!");
+        if (layer.feature && layer.feature.properties[attribute]){
+
+          //access feature properties
+           var props = layer.feature.properties;
+
+           //update each feature's radius based on new attribute values
+           var radius = calcPropRadius(props[attribute]);
+           layer.setRadius(radius);
+
+           //add city to popup content string
+           var popupContent = "<p><b>Museum:</b> " + props.Museum + "</p>";
+
+           //add formatted attribute to panel content string
+           var year = attribute.split("_")[1];
+           popupContent += "<p><b>Visitation in " + year + ":</b> " + props[attribute] + " million</p>";
+
+           //update popup with new content
+           popup = layer.getPopup();
+           popup.setContent(popupContent).update();
+
+        };
+    });
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("visitor") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+
+//Step 1: Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    //add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
+
+    //replace button content with images
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+
+    var steps = document.querySelectorAll('.step');
+
+    steps.forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
             };
 
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                   return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+
+            //Step 9: pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
+        })
+    })
+
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //Step 6: get the new index value
+        var index = this.value;
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+function getData(map){
+    //load the data
+    fetch("data/museums.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            var attributes = processData(json);
+            minValue = calcMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
         })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
